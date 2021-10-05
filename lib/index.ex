a9a5bcc7d581b9d782b7faeb9eeef351d8c18854
Brett Beatty <brettbeatty@gmail.com>
2021-10-04 19:21:51 -0600

    Generate new project

---
+++ lib/index.ex
@@ -0,0 +1,18 @@
+defmodule Index do
+  @moduledoc """
+  Documentation for `Index`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Index.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
